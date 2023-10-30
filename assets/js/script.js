ea30272fbe6fd9b931563604e053c2a58da88b26
Sam.G <samuelgerungan@gmail.com>
2023-10-30 00:08:18 -0700

    Main Ideas/Goals outline

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+//Outline for Note taking app
+
+//Main Features:
+
+//Able to take persistent/convenient on-the-fly notes or reminders
+
+//Perhaps be able to quickly rank priority? 
+
+//Able to create simple quick reminders
+
+//Able to see a quickly organized list or visual of notes/reminders
+
+//Hourly Calendar
+
+//Daily Calendar
+
+//Weekly Calendar
+
+//Monthly+ Calendar
+
+
