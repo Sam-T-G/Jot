1513429723cb350634afb02a67afc87481bef606
Sam.G <samuelgerungan@gmail.com>
2023-10-30 23:47:30 -0700

    Added more pseudocode for general functions in JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,9 @@
 //Main Features:
 
 //Able to take persistent/convenient on-the-fly notes or reminders
+//Idea is to immediately be presented with a text input box
+//Button to enter note/can use enter key to save not
+//Needs to be easy to navigate to new note or to previous notes somehow - Imperative
 
 //Perhaps be able to quickly rank priority? 
 
